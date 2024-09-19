e5a41e709a27203a2d0e539671032570e47b5490
Glenn von Wyl <glennedi_vonwyl@sluz.ch>
2024-09-19 11:07:42 +0200

    linter fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import chalk from "chalk";
 console.log(chalk.blue("It works"));
 
-let myUnusedVariable = 5;
+const myUnusedVariable = 5;
 
-if (myUnusedVariable == 5) {
+if (myUnusedVariable === 5) {
   console.log('It works');
 }
